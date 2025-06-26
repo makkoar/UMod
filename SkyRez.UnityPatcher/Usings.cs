edad47f1e63a946d8e9ca21a5bc1d82fbc2d5ca4
makkoar <makkoar@gmail.com>
2025-06-27 01:14:40 +0300

    F

--- SkyRez.UnityPatcher/Usings.cs
+++ SkyRez.UnityPatcher/Usings.cs
@@ -1,4 +1,5 @@
 ﻿global using SkyRez.Common.Managers;
+global using SkyRez.Common.Service;
 global using System.Collections.Immutable;
 global using System.Reflection;
 global using System.Reflection.Metadata;
